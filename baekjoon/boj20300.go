e0b6066ec424bfe5ca6d47e2cb614f5ff27e12e6
KyuBo-Shim <didlaak6000@naver.com>
2023-01-03 17:28:23 +0900

    solved boj20300

--- baekjoon/boj20300.go
+++ baekjoon/boj20300.go
@@ -17,6 +17,7 @@ var (
 
 func main() {
 	defer w.Flush()
+
 	fmt.Fscan(r, &N)
 	for i := 0; i < N; i++ {
 		var a int
@@ -38,3 +39,10 @@ func main() {
 	sort.Ints(temp)
 	fmt.Fprint(w, temp[len(temp)-1])
 }
+
+// 그리디로 풀었음
+// 가장 작은 수와 가장 큰 수를 더한 값이 최소 근손실이 되는 것을 이용
+// 1. 배열을 정렬한다.
+// 2. 배열의 길이가 홀수라면, 가장 큰 수를 temp에 넣는다.
+// 3. temp에는 배열의 가장 작은 수와 가장 큰 수를 더한 값이 들어간다.
+// 4. temp의 가장 큰 수를 출력한다.
